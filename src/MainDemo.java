9737dbceab6f4d608dc04903498a061a21fb14e0
ramchaulagain625 <ramchaulagain625@gmail.com>
2021-07-21 21:30:16 -0400

    Nothing changes

--- src/MainDemo.java
+++ src/MainDemo.java
@@ -2,11 +2,7 @@ public class MainDemo {
     public static void main(String[] args) {
         for(int i=20;i<30;i++){
             System.out.println("Batch 9 ");
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("Nothing changes");
         }
     }
 }
